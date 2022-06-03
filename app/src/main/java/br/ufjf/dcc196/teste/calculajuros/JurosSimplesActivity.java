a2ee61c56bc8fa56f3a932b3633b8bc61e38fc4c
Delber Soares <delberss@hotmail.com>
2022-06-03 20:14:40 -0300

    Passagem de dados da Main para Juros Simples e funcao para retornar

--- app/src/main/java/br/ufjf/dcc196/teste/calculajuros/JurosSimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/teste/calculajuros/JurosSimplesActivity.java
@@ -2,13 +2,46 @@ package br.ufjf.dcc196.teste.calculajuros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JurosSimplesActivity extends AppCompatActivity {
+
+    private EditText editTextTaxaDeJuros;
+    private EditText editTextPeriodos;
+
+    private TextView textViewResultado;
+    private TextView textViewValorPresente;
+
+    private Double valorPresente;
+    private Double valorFinal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juros_simples);
+
+        textViewValorPresente = findViewById(R.id.textViewValorPresente);
+
+        Bundle extras = getIntent().getExtras();
+        valorPresente = extras.getDouble("valorPresente");
+
+        textViewValorPresente.setText(valorPresente.toString());
+
+        editTextTaxaDeJuros = findViewById(R.id.editTextTaxaDeJuros);
+        editTextPeriodos = findViewById(R.id.editTextPeriodos);
+        textViewResultado = findViewById(R.id.textViewResultado);
+
+    }
+
+    public void retornarClick(View view){
+        Intent resultado = new Intent();
+        resultado.putExtra("valorFinal", valorFinal);
+        setResult(RESULT_OK, resultado);
+        finish();
     }
 }
--- app/src/main/java/br/ufjf/dcc196/teste/calculajuros/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/teste/calculajuros/MainActivity.java
@@ -1,14 +1,38 @@
 package br.ufjf.dcc196.teste.calculajuros;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_JUROS_SIMPLES = 1;
+
+    EditText editTextValorPresente;
+    TextView textViewValorFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextValorPresente = findViewById(R.id.editTextValorPresente);
+        textViewValorFinal = findViewById(R.id.textViewValorFinal);
+    }
+
+    public void jurosSimplesClick(View view){
+        try {
+            Double valorPresente = Double.parseDouble((editTextValorPresente.getText().toString()));
+            Intent intent = new Intent(MainActivity.this,JurosSimplesActivity.class);
+            intent.putExtra("valorPresente", valorPresente);
+            startActivityForResult(intent, REQUEST_JUROS_SIMPLES);
+        }catch (Exception e){
+            editTextValorPresente.selectAll();
+            editTextValorPresente.requestFocus();
+        }
     }
 }
